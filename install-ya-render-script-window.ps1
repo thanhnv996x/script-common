9577c4a0fbfb218fb8e39127de6838d05b99a53a
thanhnv996x <162076261+thanhnv996x@users.noreply.github.com>
2024-04-17 11:33:00 +0700

    Update install-ya-render-script-window.ps1

--- install-ya-render-script-window.ps1
+++ install-ya-render-script-window.ps1
@@ -5,7 +5,7 @@ choco install -y git
 
 choco install -y python --version=3.9.0
 
-choco install ffmpeg
+choco install -y ffmpeg
 
 Start-Process msiexec.exe -Wait -ArgumentList '/i \"https://awscli.amazonaws.com/AWSCLIV2.msi\" /q'
 
