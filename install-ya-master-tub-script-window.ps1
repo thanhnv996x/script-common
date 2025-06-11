4ab41119af9695821b1ee36ca3b99ea91cfc60a4
thanhnv996x <162076261+thanhnv996x@users.noreply.github.com>
2025-06-11 08:37:57 +0700

    Update install-ya-master-tub-script-window.ps1

--- install-ya-master-tub-script-window.ps1
+++ install-ya-master-tub-script-window.ps1
@@ -5,6 +5,8 @@ choco install -y git
 
 choco install -y python --version=3.9.0
 
+choco install python visualstudio2022-workload-vctools -y
+
 Start-Process msiexec.exe -Wait -ArgumentList '/i \"https://awscli.amazonaws.com/AWSCLIV2.msi\" /q'
 
 \$env:ChocolateyInstall = Convert-Path \"\$((Get-Command choco).Path)\..\..\"
